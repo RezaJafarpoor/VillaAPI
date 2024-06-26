4c03885244d2122699ec81949c1f53cb8a139b80
reza <rezajafarpoor222@gmail.com>
2024-06-26 05:34:53 +0330

    adding Villa entity and modify the controller

--- VillaAPI/Controllers/VillaController.cs
+++ VillaAPI/Controllers/VillaController.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VillaAPI.Entities;
 
 namespace VillaAPI.Controllers;
 [ApiController]
+[Route("api/[controller]")]
 public class VillaController : ControllerBase
 {
+    [HttpGet]
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa>
+        {
+            new Villa { Id = 1, Name = "Pool View" },
+            new Villa { Id = 2, Name = "Beach View" }
+
+        };
+    }
     
 }
