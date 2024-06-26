069a6f3d262ffcddf3d1a3fd99bd730d01feb412
reza <rezajafarpoor222@gmail.com>
2024-06-26 06:47:18 +0330

    adding Create Endpoint and Documents for it

--- VillaAPI/Controllers/VillaController.cs
+++ VillaAPI/Controllers/VillaController.cs
@@ -30,6 +30,30 @@ public class VillaController : ControllerBase
         {
             return NotFound();
         }
-        return Ok();
+        return Ok(villa);
+        
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(VillaDto))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+
+    public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villa)
+    {
+        if (villa is null)
+        {
+            return BadRequest("villa");
+        }
+
+        if (villa.Id > 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        villa.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id + 1;
+        VillaStore.villaList.Add(villa);
+        return CreatedAtAction(nameof(GetVilla), new {id = villa.Id}, villa);
     }
 }
