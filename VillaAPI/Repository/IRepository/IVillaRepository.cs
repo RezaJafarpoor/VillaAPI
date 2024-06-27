2b83ea3e6a09b3645007b5c02021af1faaf4387b
reza <rezajafarpoor222@gmail.com>
2024-06-27 18:34:12 +0330

    adding repository Pattern

---
+++ VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using VillaAPI.Entities;
+
+namespace VillaAPI.Repository.IRepository;
+
+public interface IVillaRepository
+{
+    Task Create(Villa entity);
+    Task Remove(Villa entity);
+    Task<List<Villa>> GetAll(Expression<Func<Villa,bool>>? filter = null);
+    Task<Villa> Get(Expression<Func<Villa,bool>>? filter = null, bool tracked = true);
+    Task Save();
+}
---
+++ VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using VillaAPI.Data;
+using VillaAPI.DTOs;
+using VillaAPI.Entities;
+using VillaAPI.Repository.IRepository;
+
+namespace VillaAPI.Repository;
+
+public class VillaRepository : IVillaRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public VillaRepository( ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task Create(Villa entity)
+    {
+       await _dbContext.Villas.AddAsync(entity);
+       await Save();
+    }
+
+    public async Task Remove(Villa entity)
+    {
+         _dbContext.Villas.Remove(entity);
+        await Save();
+    }
+
+    public async Task<List<Villa>> GetAll(Expression<Func<Villa,bool>>? filter = null)
+    {
+        IQueryable<Villa> query = _dbContext.Villas;
+        if (filter != null)
+        {
+            query = query.Where(filter);
+
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<Villa> Get(Expression<Func<Villa,bool>>? filter = null, bool tracked = true)
+    {
+        IQueryable<Villa> query = _dbContext.Villas;
+        if (!tracked)
+        {
+            query.AsNoTracking();
+        }
+
+        if (filter !=null)
+        {
+            query.Where(filter);
+        }
+
+        return await query.FirstOrDefaultAsync();
+    }
+
+    public async Task Save()
+    {
+       await _dbContext.SaveChangesAsync();
+    }
+}
